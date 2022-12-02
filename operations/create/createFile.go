8f45045e12653d7db51e7321b2be3e801d75c8fa
emrebdr <emrebdr29@gmail.com>
2022-12-02 21:27:56 +0300

    done createFile operation

--- operations/create/createFile.go
+++ operations/create/createFile.go
@@ -2,13 +2,50 @@ package create
 
 import (
 	"ena/dirgod/models"
-	"fmt"
+	"ena/dirgod/operations"
+	"os"
 )
 
 type CreateFile struct {
-	options models.OperationOptions
+	Path           string
+	Options        models.OperationOptions
+	Result         operations.OperationResult
+	RollbackResult operations.OperationResult
 }
 
 func (c *CreateFile) Exec() {
-	fmt.Println("Creating a file")
+	file, err := os.Create(c.Path)
+	if err != nil {
+		c.decideErrorOutput(err)
+		return
+	}
+
+	defer file.Close()
+
+	c.Result.Completed = true
+}
+
+func (c *CreateFile) Rollback() {
+	err := os.Remove(c.Path)
+	if err != nil {
+		c.RollbackResult.Completed = false
+		c.RollbackResult.Err = err
+		return
+	}
+
+	c.RollbackResult.Completed = true
+}
+
+func (c *CreateFile) decideErrorOutput(err error) {
+	if c.Options.WorkingMode == models.Force {
+		c.Result.Completed = true
+		c.Result.Err = err
+	} else if c.Options.WorkingMode == models.Default {
+		// TODO: check for dependency tree
+		c.Result.Completed = false
+		c.Result.Err = err
+	} else {
+		c.Result.Completed = false
+		c.Result.Err = err
+	}
 }
