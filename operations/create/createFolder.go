513056fd03f52fe6fbdc1e8328caf978621810c5
emrebdr <emrebdr29@gmail.com>
2022-12-02 21:37:19 +0300

    done create folder operation

--- operations/create/createFolder.go
+++ operations/create/createFolder.go
@@ -2,13 +2,56 @@ package create
 
 import (
 	"ena/dirgod/models"
-	"fmt"
+	"ena/dirgod/operations"
+	"os"
 )
 
+var DEFAUL_CREATE_FOLDER_PERMISSION = os.FileMode(0755)
+
 type CreateFolder struct {
-	options models.OperationOptions
+	Path           string
+	Options        models.OperationOptions
+	Result         operations.OperationResult
+	RollbackResult operations.OperationResult
 }
 
 func (c *CreateFolder) Exec() {
-	fmt.Println("Creating a folder")
+	var err error
+	if c.Options.WorkingMode == models.Force {
+		err = os.MkdirAll(c.Path, DEFAUL_CREATE_FOLDER_PERMISSION)
+	} else {
+		err = os.Mkdir(c.Path, DEFAUL_CREATE_FOLDER_PERMISSION)
+	}
+
+	if err != nil {
+		c.decideErrorOutput(err)
+		return
+	}
+
+	c.Result.Completed = true
+}
+
+func (c *CreateFolder) Rollback() {
+	// Remove single empty folder or force remove all recursive child folders and files with checking WorkingMode
+	err := os.RemoveAll(c.Path)
+	if err != nil {
+		c.RollbackResult.Completed = false
+		c.RollbackResult.Err = err
+		return
+	}
+
+	c.RollbackResult.Completed = true
+}
+
+func (c *CreateFolder) decideErrorOutput(err error) {
+	if c.Options.WorkingMode == models.Force {
+		c.Result.Completed = true
+		c.Result.Err = err
+	} else if c.Options.WorkingMode == models.Default {
+		c.Result.Completed = false
+		c.Result.Err = err
+	} else {
+		c.Result.Completed = false
+		c.Result.Err = err
+	}
 }
