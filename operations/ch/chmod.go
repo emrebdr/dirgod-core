4177d4e043fdd86009966d44414ebbf2672ecaae
emrebdr <emrebdr29@gmail.com>
2022-12-03 00:20:03 +0300

    chmod and chown operations done

---
+++ operations/ch/chmod.go
@@ -0,0 +1,90 @@
+package ch
+
+import (
+	"ena/dirgod/models"
+	"ena/dirgod/operations"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type Chmod struct {
+	Source           string
+	PermCode         fs.FileMode
+	PreviousPermCode fs.FileMode
+	Recursive        bool
+	Options          models.OperationOptions
+	Result           operations.OperationResult
+	RollbackResult   operations.OperationResult
+}
+
+func (c *Chmod) Exec() {
+	stats, err := os.Stat(c.Source)
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return
+	}
+
+	c.PreviousPermCode = stats.Mode()
+
+	if c.Recursive {
+		err = filepath.WalkDir(c.Source, c.chmodRecursive)
+	} else {
+		err = os.Chmod(c.Source, c.PermCode)
+	}
+
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return
+	}
+
+	c.Result.Completed = true
+}
+
+func (c *Chmod) Rollback() {
+	var err error
+
+	if c.Recursive {
+		err = filepath.WalkDir(c.Source, c.chmodRecursiveRollback)
+	} else {
+		err = os.Chmod(c.Source, c.PreviousPermCode)
+	}
+
+	if err != nil {
+		c.RollbackResult.Completed = false
+		c.RollbackResult.Err = err
+		return
+	}
+
+	c.RollbackResult.Completed = true
+}
+
+func (c *Chmod) chmodRecursive(path string, de fs.DirEntry, err error) error {
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return err
+	}
+
+	err = os.Chmod(path, c.PermCode)
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *Chmod) chmodRecursiveRollback(path string, de fs.DirEntry, err error) error {
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return err
+	}
+
+	err = os.Chmod(path, c.PreviousPermCode)
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return err
+	}
+
+	return nil
+}
---
+++ operations/ch/chown.go
@@ -0,0 +1,92 @@
+package ch
+
+import (
+	"ena/dirgod/models"
+	"ena/dirgod/operations"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type Chown struct {
+	Source         string
+	UID            int
+	GID            int
+	PreviousUID    int
+	PreviousGID    int
+	Recursive      bool
+	Options        models.OperationOptions
+	Result         operations.OperationResult
+	RollbackResult operations.OperationResult
+}
+
+func (c *Chown) Exec() {
+	_, err := os.Stat(c.Source)
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return
+	}
+
+	c.PreviousGID = os.Getgid()
+	c.PreviousUID = os.Getuid()
+
+	if c.Recursive {
+		err = filepath.WalkDir(c.Source, c.chownRecursive)
+	} else {
+		err = os.Chown(c.Source, c.UID, c.GID)
+	}
+
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return
+	}
+
+	c.Result.Completed = true
+}
+
+func (c *Chown) Rollback() {
+	var err error
+	if c.Recursive {
+		err = filepath.WalkDir(c.Source, c.chownRecursiveRollback)
+	} else {
+		err = os.Chown(c.Source, c.PreviousUID, c.PreviousGID)
+	}
+
+	if err != nil {
+		c.RollbackResult.Completed = false
+		c.RollbackResult.Err = err
+		return
+	}
+
+	c.RollbackResult.Completed = true
+}
+
+func (c *Chown) chownRecursive(path string, de fs.DirEntry, err error) error {
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return err
+	}
+
+	err = os.Chown(path, c.UID, c.GID)
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *Chown) chownRecursiveRollback(path string, de fs.DirEntry, err error) error {
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return err
+	}
+
+	err = os.Chown(path, c.PreviousUID, c.PreviousGID)
+	if err != nil {
+		operations.DecideErrorOutput(&c.Options, &c.Result, err)
+		return err
+	}
+
+	return nil
+}
